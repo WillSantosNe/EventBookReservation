0660825fb75a87a52dcb70369616a596f1b72c3a
WillSantosNe <williamsantosstella@gmail.com>
2025-03-09 13:10:04 -0300

    feat(user): Create User Repository

---
+++ src/main/java/project/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package project.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import project.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	/**
+	 * Finds a user by their email address.
+	 *
+	 * @param email the email address to search for.
+	 * @return an Optional containing the found user, or an empty Optional if no
+	 *         user found.
+	 */
+	Optional<User> findByEmail(String email);
+}
