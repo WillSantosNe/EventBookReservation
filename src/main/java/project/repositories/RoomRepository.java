cb8f57d5a5049bd5145736045a36be659999b429
WillSantosNe <williamsantosstella@gmail.com>
2025-03-15 14:31:20 -0300

    feat(room): Adding @EntityGraph to avoid LazyInitializationException when loading the list of Bookings

--- src/main/java/project/repositories/RoomRepository.java
+++ src/main/java/project/repositories/RoomRepository.java
@@ -1,5 +1,8 @@
 package project.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,6 @@ import project.entities.Room;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+	@EntityGraph(attributePaths = "bookings") 
+    Optional<Room> findById(Long id);
 }
