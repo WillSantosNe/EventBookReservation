4d46051fd590c9ac25ed3e7925662ac79b20132c
WillSantosNe <williamsantosstella@gmail.com>
2025-03-26 22:08:38 -0300

    feat(auth): add public registration endpoint using UserService

--- src/main/java/project/controllers/AuthController.java
+++ src/main/java/project/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import project.dto.auth.AuthRequestDTO;
 import project.dto.auth.AuthResponseDTO;
+import project.dto.user.CreateUserDTO;
+import project.dto.user.UserResponseDTO;
 import project.services.AuthService;
+import project.services.UserService;
 
 /**
  * Controller responsible for managing authentication endpoints.
@@ -23,6 +27,9 @@ public class AuthController {
 	@Autowired
 	private AuthService authService;
 
+	@Autowired
+	private UserService userService;
+
 	/**
 	 * Endpoint for user login.
 	 *
@@ -33,5 +40,20 @@ public class AuthController {
 	@PostMapping("/login")
 	public AuthResponseDTO login(@RequestBody AuthRequestDTO dto) {
 		return authService.authenticate(dto);
+	}
+
+	/**
+	 * Handles user registration requests by accepting a CreateUserDTO object
+	 * and returning a UserResponseDTO object.
+	 *
+	 * @param dto The data transfer object containing user creation details.
+	 * @return ResponseEntity containing the UserResponseDTO with the created user's information.
+	 * 
+	 * @see CreateUserDTO
+	 * @see UserResponseDTO
+	 */
+	@PostMapping("/register")
+	public ResponseEntity<UserResponseDTO> register(@RequestBody CreateUserDTO dto) {
+	    return ResponseEntity.ok(userService.createUser(dto));
 	}
 }
